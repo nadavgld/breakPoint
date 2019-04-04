161d5172c1f63e1eedec847f031fab780d24e596
Shai Katz <shai@soluto.com>
2019-04-05 02:48:26 +0300

    notify rooms

--- server/services/socket.js
+++ server/services/socket.js
@@ -1,17 +1,19 @@
 var connectedUsers = {};
-
+var _io = io;
 export default (io) => {
+  _io = io;
   io.on('connection', function (socket) {
     socket.on('register', function (data) {
-      connectedUsers[data.email]= socket;
+      connectedUsers[data.email] = socket;
     });
   });
 }
 
 export const addToRoom = (email, roomId) => {
   connectedUsers[email].join(roomId);
+  _io.to(roomId).emit('new_user_in_room', email);
 }
 
 export const removeFromRoom = (email, roomId) => {
   connectedUsers[email].leave(roomId);
-}
+  _io.to(roomId).emit('user_left_room', email);
