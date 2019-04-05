1f22af4b47ddd7751ebb43eecceab32efb198ac4
Shai Katz <shai@soluto.com>
2019-04-05 04:02:44 +0300

    add logs

--- server/services/socket.js
+++ server/services/socket.js
@@ -6,16 +6,30 @@ module.exports = {
       _io = io;
       _io.origins('*:*');
       io.on('connection', function (socket) {
+        console.log('connection established', socket.id)
         socket.on('register', function (data) {
+          console.log('socket registered', data)
           connectedUsers[data.email] = socket;
+        });
+        socket.on('disconnect', (reason) => {
+          console.log('socket disconnected', reason)
+          Object.keys(connectedUsers).forEach(user => {
+            if (connectedUsers[user].id == socket.id) {
+              delete connectedUsers[user];
+            }
+          });
         });
       });
    },
    addToRoom: (email, roomId) => {
+    console.log(`adding ${email} to room ${roomId}`);
+    console.log('connected users', connectedUsers);
     connectedUsers[email].join(roomId);
     _io.to(roomId).emit('new_user_in_room', email);
    },
    removeFromRoom: (email, roomId) => {
+    console.log(`removing ${email} from room ${roomId}`);
+    console.log('connected users', connectedUsers);
     connectedUsers[email].leave(roomId);
     _io.to(roomId).emit('user_left_room', email);
    }
